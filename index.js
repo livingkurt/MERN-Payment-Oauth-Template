d5dd79a42b3f3b865d598786dd1953dd8cd8a145
Kurt LaVacque <lavacquek@icloud.com>
2020-02-11 18:56:41 -0600

    Working on Getting mongoose working with oauth

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-require('./services/passport')
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
+
+mongoose.connect(keys.mongoURI);
+
+require('./models/user')
+require('./services/passport')
 
 const app = express();
 
 require('./routes/authRoutes.js')(app);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
