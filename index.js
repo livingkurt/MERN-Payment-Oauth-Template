3aa5cf00810b8778bd539f9d28027f45d27bef8e
Kurt LaVacque <lavacquek@icloud.com>
2020-02-11 21:05:51 -0600

    I thought I fixed the problem

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
+require('./models/User')
+require('./services/passport')
 
 mongoose.connect(keys.mongoURI);
-
-require('./models/user')
-require('./services/passport')
 
 const app = express();
 
