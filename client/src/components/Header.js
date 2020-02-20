ed73b78dcfef692442c10c7667b8b7b5f13ed500
Kurt LaVacque <lavacquek@icloud.com>
2020-02-20 09:45:15 -0600

    Trying to fix merge conflicts

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-<<<<<<< HEAD
 import Payments from './Payments'
-=======
->>>>>>> 7927c19a9d9bc7e99cc9cbd37896f1fb15a028b8
 
 class Header extends Component {
 
@@ -17,13 +14,9 @@ class Header extends Component {
                     <li><a href="/auth/google">Login With Google</a></li>
                 )
             default:
-<<<<<<< HEAD
                 return [
                 <li><Payments /></li>,
                 <li><a href="/api/logout">Logout</a></li>]
-=======
-                return <li><a href="/api/logout">Logout</a></li>
->>>>>>> 7927c19a9d9bc7e99cc9cbd37896f1fb15a028b8
         }
     }
 
