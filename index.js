c432edd9bb8720c840f45abd3534921b6b1302a3
Kurt LaVacque <lavacquek@icloud.com>
2020-02-25 12:42:50 -0600

    Added billing and client side app

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 )
 
 app.use(passport.initialize());
@@ -25,6 +25,14 @@ app.use(passport.session());
 
 require('./routes/authRoutes.js')(app);
 require('./routes/billingRoutes.js')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
